2f92aa7a8bf717b7c4381b2d7a2e5539d60020a3
AvichalChadha <avichalchadha1999@gmailcom>
2018-02-20 10:27:28 +0530

    code seems to work, but the loader is not shwoing up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { loadingBarReducer } from 'react-redux-loading-bar'
 
 //REDUCERS 
 
@@ -7,7 +8,8 @@ import news from './allNews'
 
 const rootReducer = combineReducers({
    
-    news
+    news,
+    loadingBar: loadingBarReducer
 
 });
 
